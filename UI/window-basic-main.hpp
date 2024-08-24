bf64f10443464b9a20d291dce9a09b3c3795461f
cg2121 <claytong1214@gmail.com>
2024-08-23 23:08:07 -0500

    UI: Add preview zoom controls
    This adds preview zoom out and zoom in buttons next the the scaling
    percent label. This also adds these as actions in the scaling menu,
    along with a reset zoom action.

--- UI/window-basic-main.hpp
+++ UI/window-basic-main.hpp
@@ -680,6 +680,8 @@ private:
 
 	bool restartingVCam = false;
 
+	void UpdateZoomControls();
+
 public slots:
 	void DeferSaveBegin();
 	void DeferSaveEnd();
@@ -815,6 +817,13 @@ private slots:
 
 	void on_previewXScrollBar_valueChanged(int value);
 	void on_previewYScrollBar_valueChanged(int value);
+	void on_actionPreviewResetZoom_triggered();
+	void on_actionPreviewZoomOut_triggered();
+	void on_actionPreviewZoomIn_triggered();
+	void on_previewZoomOutButton_clicked();
+	void on_previewZoomInButton_clicked();
+	void on_preview_ZoomIsMinimum();
+	void on_preview_ZoomIsMaximum();
 
 	void PreviewScalingModeChanged(int value);
 
@@ -1304,6 +1313,9 @@ signals:
 	/* Preview signals */
 	void PreviewXScrollBarMoved(int value);
 	void PreviewYScrollBarMoved(int value);
+	void PreviewResetZoom();
+	void PreviewZoomOut();
+	void PreviewZoomIn();
 
 private:
 	std::unique_ptr<Ui::OBSBasic> ui;
